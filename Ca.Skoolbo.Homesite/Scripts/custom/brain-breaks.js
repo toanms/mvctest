6b9c2faf5e2a8507bce8d6c2ccbebdc06686fb88
Huynh Viet An <an.huynh@skoolbo.com.vn>
2016-11-22 17:08:54 +0700

    change layout

--- Ca.Skoolbo.Homesite/Scripts/custom/brain-breaks.js
+++ Ca.Skoolbo.Homesite/Scripts/custom/brain-breaks.js
@@ -1,9 +1,33 @@
+
+var get_params = function (search_string) {
+
+    var parse = function (params, pairs) {
+        var pair = pairs[0];
+        var parts = pair.split('=');
+        var key = decodeURIComponent(parts[0]);
+        var value = decodeURIComponent(parts.slice(1).join('='));
+
+        // Handle multiple parameters of the same name
+        if (typeof params[key] === "undefined") {
+            params[key] = value;
+        } else {
+            params[key] = [].concat(params[key], value);
+        }
+
+        return pairs.length == 1 ? params : parse(params, pairs.slice(1))
+    }
+
+    // Get rid of leading ?
+    return search_string.length == 0 ? {} : parse({}, search_string.substr(1).split('&'));
+}
+
 var myOptions = {
-	"nativeControlsForTouch": false,
-	controls: true,
-	autoplay: false,
-	width: "100%",
-	height: "345",
+    "nativeControlsForTouch": false,
+    techOrder: ["azureHtml5JS", "html5", "flashSS", "html5FairPlayHLS", "silverlightSS"],
+    controls: true,
+    autoplay: false,
+    width: "100%",
+    height: "345",
     hotKeys: {
         enableFullscreen: false
     }
@@ -17,13 +41,29 @@ myPlayer.src([
         }
 ]);
 
- $(document).on("click", ".show-video", function (e) {
-    $("#myVideo").modal("show");
+$(document).ready(function () {
 
-  $('#myVideo').on('shown.bs.modal', function (e) {
+    var params = get_params(location.search);
+    var autoPlay = params['autoplay'];
+
+    if (autoPlay != null && autoPlay != undefined) {
+        $("#myVideo").modal("show");
+        myPlayer.ready(function () {
+            this.play();
+        });
+    }
+
+
+    $('#myVideo').on('shown.bs.modal', function (e) {
         myPlayer.play();
     });
     $('#myVideo').on('hidden.bs.modal', function (e) {
         myPlayer.pause();
     });
+
+    $(document).on("click", ".show-video", function (e) {
+        $("#myVideo").modal("show");
+    });
 });
+
+
