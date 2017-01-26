f20d67b6a46575428a13622e39ef58ab5440a440
Toan Nguyen <toanms@hotmail.com>
2017-01-25 17:58:32 -0800

    test

---
+++ Scripts/src/app/main.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from "@angular/core"
+
+@Component({
+    moduleId: module.id,
+    selector: "main-component",
+    templateUrl: "main-template.html"
+})
+export class MainComponent {
+    
+}
